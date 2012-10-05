7a78c5dfc7c8aca80bb8ebdcae677fe53680dacd
Qing-Cheng Li <qc.linux@gmail.com>
2012-10-05 15:33:31 +0800

    yo

--- shot_detect.py
+++ shot_detect.py
@@ -12,10 +12,9 @@ import sys,os,math
 
 def main(difffile,answer):
     df = open(difffile,'r')
-    #df = open(answer,'r')
 
-    rgb_th = 10000
-    hsv_th = 40000
+    rgb_th = 20000
+    hsv_th = 38000
     yiq_th = 20000
 
     rgb = []
@@ -23,10 +22,18 @@ def main(difffile,answer):
     yiq = []
     path = []
 
-    shot = []
+    shot_h = []
+    shot_r = []
+    shot_y = []
 
-    cur_s = -1
-    cur_e = -1
+    cur_s_h = -1
+    cur_e_h = -1
+
+    cur_s_r = -1
+    cur_e_r = -1
+
+    cur_s_y = -1
+    cur_e_y = -1
 
     ws = 5
 
@@ -40,56 +47,102 @@ def main(difffile,answer):
     for i in range(0,len(rgb)):
         
         if hsv[i]>hsv_th:
-            if i > cur_e and i > cur_s:
-                cur_s = i
+            if i > cur_e_h and i > cur_s_h:
+                cur_s_h = i
                 k = i + ws
             
                 if k > len(rgb):
                     k = len(rgb)
 
                 for j in range(i,k):
-                    if hsv[j] > hsv_th and j > cur_e:
-                        cur_e = j
+                    if hsv[j] > hsv_th and j > cur_e_h:
+                        cur_e_h = j
           
-            if i == cur_e:
+            if i == cur_e_h:
                 e = False
                 k = i + ws
                 if k > len(rgb):
                     k = len(rgb)
                 
                 for j in range(i,k):
-                    if hsv[j] > hsv_th and j > cur_e:
+                    if hsv[j] > hsv_th and j > cur_e_h:
                         e = True
-                        cur_e = j
+                        cur_e_h = j
 
                 if not e:
-                    shot.append((cur_s,cur_e))
+                    shot_h.append((cur_s_h,cur_e_h))
          
-
-    for pair in shot:
-        print pair
-        #r = False
-        #h = False
-        #y = False
-
-        #if(rgb>rgb_th):
-        #    r = True
-        #if(hsv>hsv_th):
-        #    h = True
-        #if(yiq>yiq_th):
-        #    y = True
-
-        #if r or h or y:
-        #    print frame,
-        #    if r:
-        #        print 'RGB',
-        #    if h:
-        #        print 'HSV',
-        #    if y:
-        #        print 'YIQ',
-        #    print ''
+        if rgb[i]>rgb_th:
+            if i > cur_e_r and i > cur_s_r:
+                cur_s_r = i
+                k = i + ws
             
+                if k > len(rgb):
+                    k = len(rgb)
 
+                for j in range(i,k):
+                    if rgb[j] > rgb_th and j > cur_e_r:
+                        cur_e_r = j
+          
+            if i == cur_e_r:
+                e = False
+                k = i + ws
+                if k > len(rgb):
+                    k = len(rgb)
+                
+                for j in range(i,k):
+                    if rgb[j] > rgb_th and j > cur_e_r:
+                        e = True
+                        cur_e_r = j
+
+                if not e:
+                    shot_r.append((cur_s_r,cur_e_r))
+         
+        if yiq[i]>yiq_th:
+            if i > cur_e_y and i > cur_s_y:
+                cur_s_y = i
+                k = i + ws
+            
+                if k > len(rgb):
+                    k = len(rgb)
+
+                for j in range(i,k):
+                    if yiq[j] > yiq_th and j > cur_e_y:
+                        cur_e_y = j
+          
+            if i == cur_e_y:
+                e = False
+                k = i + ws
+                if k > len(rgb):
+                    k = len(rgb)
+                
+                for j in range(i,k):
+                    if yiq[j] > yiq_th and j > cur_e_y:
+                        e = True
+                        cur_e_y = j
+
+                if not e:
+                    shot_y.append((cur_s_y,cur_e_y))
+         
+    lh = len(shot_h)
+    lr = len(shot_r)
+    ly = len(shot_y)
+
+    for i in range(0,max(lh,max(lr,ly))):
+        if i < lh:
+            print shot_h[i],'\t',
+        else:
+            print "       ",'\t',
+
+        if i < lr:
+            print shot_r[i],'\t',
+        else:
+            print "       ",'\t',
+
+        if i < ly:
+            print shot_y[i]
+        else:
+            print "       "
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
